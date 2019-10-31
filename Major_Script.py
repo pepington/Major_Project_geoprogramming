1a6391b3771251414a1739132a4505f64b447ae0
pepington <s3604797@student.rmit.edu.au>
2019-10-31 21:55:30 +1100

    Update Major_Script.py

--- Major_Script.py
+++ Major_Script.py
@@ -17,7 +17,7 @@ planningLayer = iface.addVectorLayer(filePath + planning_fileName , planning_fil
 areaLayer = iface.addVectorLayer(filePath + area_fileName , area_fileName[:-4], "ogr")
 
 #Clip planning scheme by area 
-clipOutput = filePath + "clipOutput_45.shp"
+clipOutput = filePath + "clipOutput_57.shp"
 
 clipdict = { "INPUT" : planningLayer, 
               "OVERLAY" : areaLayer  ,
@@ -42,8 +42,10 @@ clippedLayer.updateFields()
 features = clippedLayer.getFeatures()
 
 idx = clippedLayer.fields().indexOf('ZONE_CODE')
+
 area_index = f.fieldNameIndex('AREA')
 
+atot = 0.0 
 for f in features: 
     
     geom = f.geometry().area()
@@ -55,6 +57,10 @@ for f in features:
     clippedLayer.updateFeature(f)
     
 #Fill in ZONE CODE field with code minus numbers 
+    area_attr = f.attributes()[idx_a]
+    
+    atotal = atot + area_attr
+    
     
     attbs = f.attributes()[idx]
     
@@ -71,39 +77,41 @@ for f in features:
     
     clippedLayer.updateFeature(f)
 
- 
 clippedLayer.commitChanges()
+print (atotal)
 
 #Sum area values of polygons with the same zone code
 
 code_idx = clippedLayer.fields().indexOf('CODE')
 
 zone_codes = clippedLayer.uniqueValues(code_idx)
-print (zone_codes)
+#print (zone_codes)
     
 idx_a = clippedLayer.fields().indexOf('AREA')
- 
+
+
+
 for uv in zone_codes :
          
 # statement to select only features with same zone code 
-#    sum = 0 
-#    exp = QgsExpression("CODE =" + uv)
-#    print (exp)
+
     expression = ('"CODE" = ' + "'" + uv +"'")
-    print(expression)
+#    print(expression)
     request = QgsFeatureRequest().setFilterExpression(expression)
 #   print (request)
     
 # get features with same zone code    
     featforuv = clippedLayer.getFeatures(request)
-    print (featforuv)
+#    print (featforuv)
     total = 0.0
       
     for a in featforuv:
         attributes = a.attributes()[idx_a]
-        print (attributes)
-        total = int(attributes)
-        print(total)
+#        print (attributes)
         
-    print("ZONE:",uv, "Total area:",)
+        total = total + attributes
+        
+#        print('total:', total)
+       
+    print("ZONE:",uv, "Total area:", total/1000000)
     
